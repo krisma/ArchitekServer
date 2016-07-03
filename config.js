4a57e3df93a6e11836ad8f34f4d687f6434edbf2
Kris Ma <weijian.ma@berkeley.edu>
2016-07-02 23:06:11 -0700

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,7 @@ module.exports = {
 	  'secureUpload': 'wearethechampions',
     'secureLogin': 'igottafeeling',
     // 'database': 'mongodb://localhost:8888',
-    'database': 'mongodb://krisma:success@ds055822.mongolab.com:55822/heroku_888zzzpx',
-    'crypto': 'NOIFDnw9fnPejf0saefjme0', // digest crypto
+
     // 'userDefault': {
     // 	'groupId': null,
   		// 'groupInviteIds': [],
